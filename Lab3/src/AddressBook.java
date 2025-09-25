8299ad828aa308e227f3396365b1c723e452831d
pardis-21 <pardisehsani@cmail.carleton.ca>
2025-09-25 08:01:21 -0400

    testing

---
+++ Lab3/src/AddressBook.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+
+    private static ArrayList<BuddyInfo> addressBook;
+
+    private AddressBook() {
+        this.addressBook = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        addressBook.add(buddy);
+    }
+    public void removeBuddy(BuddyInfo buddy){
+        addressBook.remove(buddy);
+    }
+
+    public static void main(String[] args) {
+
+        BuddyInfo buddy = new BuddyInfo("TOM", "SMITH", 22);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+        System.out.println("Address Book");
+        System.out.println(addressBook);
+    }
+
+}
